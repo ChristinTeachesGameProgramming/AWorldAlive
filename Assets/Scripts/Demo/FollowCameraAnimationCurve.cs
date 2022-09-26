8d0eee2a62e4ea732d5fb47d5923f8471134fa95
Christin Kalina <christin.kalina@ringtailtechnology.com>
2022-09-26 09:33:51 +0200

    Fix animation curve

--- Assets/Scripts/Demo/FollowCameraAnimationCurve.cs
+++ Assets/Scripts/Demo/FollowCameraAnimationCurve.cs
@@ -2,23 +2,41 @@ using UnityEngine;
 
 public class FollowCameraAnimationCurve : MonoBehaviour
 {
-    [SerializeField] private float _smoothness = 1f;
+    [SerializeField][Range(0.1f, 1)] private float _speed = 1f;
     [SerializeField] private AnimationCurve _animationCurve;
     public Transform Player;
 
-    private Vector3 initalOffset;
-    private Vector3 cameraPosition;
+    private Vector3 _initalOffset;
 
+    private Vector3 _startPosition;
+    private Vector3 _targetPosition;
+    private Vector3 _lastPlayerPosition;
+    private float _animationTime;
 
     void Start()
-    {  
-        initalOffset = transform.position - Player.position;
+    {
+        _initalOffset = transform.position - Player.position;
+        UpdatePath();
     }
 
     void FixedUpdate()
     {
-        cameraPosition = Player.position + initalOffset;
-        var step = _animationCurve.Evaluate(Time.fixedDeltaTime) * _smoothness;
-        transform.position = Vector3.Lerp(transform.position, cameraPosition, step);
+        if (Player.position != _lastPlayerPosition)
+        {
+            UpdatePath();
+        }
+
+        _animationTime += Time.deltaTime;
+
+        var step = _animationCurve.Evaluate(_animationTime * 1 / _speed);
+        transform.position = Vector3.Lerp(_startPosition, _targetPosition, step);
+    }
+
+    private void UpdatePath()
+    {
+        _startPosition = transform.position;
+        _targetPosition = Player.position + _initalOffset;
+        _lastPlayerPosition = Player.position;
+        _animationTime = 0;
     }
 }
